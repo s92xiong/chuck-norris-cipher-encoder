4f96f4a17b6f94abba432f08238a4696b86fd8b0
Sheuh Xiong <sheuhxiong@gmail.com>
2023-03-11 21:55:16 -0500

    Stage 1
    Remove Main class

--- src/main/java/stage1/Cipher.java
+++ src/main/java/stage1/Cipher.java
@@ -1,6 +1,17 @@
 package stage1;
+
+import java.util.Scanner;
+
 public class Cipher {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string:");
+        String myString = scanner.nextLine();
+        String result = "";
+        System.out.println();
+        for (int i = 0; i < myString.length(); i++) {
+            result += myString.charAt(i) + " ";
+        }
+        System.out.println(result);
     }
 }
