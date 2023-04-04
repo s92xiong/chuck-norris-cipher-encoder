c04f43d917832c05561804a2b6e9e1f4a2118152
Sheuh Xiong <sheuhxiong@gmail.com>
2023-04-04 13:09:52 -0400

    Clean up

--- src/main/java/stage4/Decipher.java
+++ src/main/java/stage4/Decipher.java
@@ -12,50 +12,32 @@ public class Decipher {
         System.out.println();
         System.out.println("The result:");
 
-        // Split the ciphered string at the space --> ["0", "0", "00", "0000"]
-        String[] cipherString = input.split(" ");
+        // Split ciphered input at the space --> ["0", "0", "00", "0000"]
+        String[] cipherStringArr = input.split(" ");
 
-        // Create a list containing binary chunks
-        List<String> binaryChunkList = new ArrayList<>();
+        // Decipher the array of 0s into binary chunks --> ["1", "0000"]
+        List<String> binaryChunkList = decipherIntoBinaryChunks(cipherStringArr);
 
-        // Declare a string builder that will have the binary chunks appended to it
+        // Create a string builder and append the binary chunks onto it
         StringBuilder binaryStrBuilder = new StringBuilder();
-
-        for (int i = 0; i < cipherString.length; i+=2) {
-            char header = (cipherString[i].equals("0")) ? '1' : '0';
-            int strlen = cipherString[i + 1].length();
-
-            String binaryChunk = duplicateChar(header, strlen);
-            binaryChunkList.add(binaryChunk);
-        }
-
-        // Append chunks to binaryStringBuilder
         binaryChunkList.forEach(binaryStrBuilder::append);
 
         // Split the binary string into substrings, e.g. "10000111000011" --> ["1000011", "1000011"]
         List<String> sevenDigitBinaryChunks = splitStringIntoSubstrings(binaryStrBuilder.toString(), 7);
 
-        // Print result
+        // Print string result
         System.out.println(binaryToChar(sevenDigitBinaryChunks));
     }
 
-    private static String binaryToChar(List<String> binaryChunkList) {
-        StringBuilder finalStrBuilder = new StringBuilder();
-        for (String chunk : binaryChunkList) {
-            int decimalVal = Integer.parseInt(chunk, 2);
-            char character = (char) decimalVal;
-            finalStrBuilder.append(character);
+    private static List<String> decipherIntoBinaryChunks(String[] arr) {
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i+=2) {
+            char header = (arr[i].equals("0")) ? '1' : '0';
+            int strlen = arr[i + 1].length();
+            String binaryChunk = duplicateChar(header, strlen);
+            stringList.add(binaryChunk);
         }
-        return finalStrBuilder.toString();
-    }
-
-    private static List<String> splitStringIntoSubstrings(String input, int substringLength) {
-        List<String> substrings = new ArrayList<>();
-        for (int i = 0; i < input.length(); i += substringLength) {
-            String substring = input.substring(i, Math.min(i + substringLength, input.length()));
-            substrings.add(substring);
-        }
-        return substrings;
+        return stringList;
     }
 
     public static String duplicateChar(char c, int x) {
@@ -65,4 +47,29 @@ public class Decipher {
         }
         return result;
     }
+
+    private static List<String> splitStringIntoSubstrings(String input, int substringLength) {
+        List<String> substrings = new ArrayList<>();
+        for (int i = 0; i < input.length(); i += substringLength) {
+            // Using Math.min is not necessary because the input.length should always be divisible by substringLength
+            // It ensures we don't extract a substring that goes beyond the end of the input string
+            // However, it is nice to have as a safety measure
+            String substring = input.substring(i, Math.min(i + substringLength, input.length()));
+            substrings.add(substring);
+        }
+        return substrings;
+    }
+
+    private static String binaryToChar(List<String> binaryChunkList) {
+        StringBuilder finalStrBuilder = new StringBuilder();
+        for (String chunk : binaryChunkList) {
+            // The 2 as the second argument specifies that the string should be interpreted as a binary number
+            int decimalVal = Integer.parseInt(chunk, 2);
+            // Cast decimalVal to a char to get the corresponding ASCII character
+            char character = (char) decimalVal;
+            finalStrBuilder.append(character);
+        }
+        return finalStrBuilder.toString();
+    }
+
 }
